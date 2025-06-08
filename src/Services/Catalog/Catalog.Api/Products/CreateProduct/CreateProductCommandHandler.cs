5fdc19402504eb793045ee900e71832e10bca5dd
mohsen.bahrzadeh@gmail.com <mohsen.bahrzadeh@gmail.com>
2025-06-08 07:07:00 +0330

    Add CQRS

---
+++ src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+
+namespace Catalog.Api.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, 
+                                   List<string> Categories, 
+                                   string Description,
+                                   string ImageFile,
+                                   decimal Price):IRequest<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+
+public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
+
