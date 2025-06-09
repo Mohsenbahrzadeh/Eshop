918d38e37aea182f7c2e12feba8c935a2f4072a3
MohsenBahrzadeh <mohsen.bahrzadeh@gmail.com>
2025-06-09 11:24:11 +0000

    Add BuildingBlock to catalog service

--- src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductCommandHandler.cs
+++ src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -1,17 +1,17 @@
 ﻿using MediatR;
+using  BuildingBlocks.CQRS;
 
 namespace Catalog.Api.Products.CreateProduct;
-
-public record CreateProductCommand(string Name, 
+public record CreateProductCommand(string Name,
                                    List<string> Categories, 
                                    string Description,
                                    string ImageFile,
-                                   decimal Price):IRequest<CreateProductResult>;
+                                   decimal Price):ICommand<CreateProductResult>;
 
 public record CreateProductResult(Guid Id);
 
 
-public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+public class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
     {
